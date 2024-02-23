0df5cdc9ed7f2d9f4c6ee3706d3a48359407cb3a
devdynam0507 <wsnam0507@naver.com>
2024-02-23 15:54:24 +0900

    chore: initial commit

---
+++ applications/api-server/src/main/java/com/gamzabit/api/GamzabitApiApplication.java
@@ -0,0 +1,12 @@
+package com.gamzabit.api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GamzabitApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(GamzabitApiApplication.class, args);
+    }
+}
